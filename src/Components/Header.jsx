93d578d006a22423999cdbea2c4d96a77abe99ed
iamkodenaime <creativedannyk@gmail.com>
2024-11-02 22:12:43 +0100

    fnished routing the blog

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../Assets/logo.png'
+import { FaWindowClose } from "react-icons/fa";
+import { FaBarsProgress } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <div className="container nav-container">
+        <Link to="/" className='logo'>
+          <img src={logo} alt="logo of the website" />
+        </Link>
+      </div>
+
+      <ul className="nav-menu">
+        <li><Link to="/profile">Kode</Link></li>
+        <li><Link to="/create">Create Post</Link></li>
+        <li><Link to="/authors">Authors</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
+      </ul>
+
+      <button className="toggle-btn">
+        <FaBarsProgress />
+      </button>
+
+    </nav>
   )
 }
 
